69edff4179269866e7bf4d10ace280b68576ba7e
Jaysus55 <oogiewoogie0@gmail.com>
2023-04-27 21:33:15 -0400

    finition du 2.2b
    ajout des animations
    polissage de 2.2a
    ajout de séparateurs de code

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-gsap.from(".CTA" , {
+var CTA = gsap.from(".CTA" , {
     y:-50,
     opacity:0,
     duration:2,
@@ -12,17 +12,84 @@ gsap.from(".CTA" , {
   })  
 
 
-/*let body = document.querySelector(".scroll");
- 
 
-window.addEventListener("scroll", function(){
-body.classList.add("is-scrolling");
-window.clearTimeout(timer);
-timer = setTimeout(removeClass, 100);
-});
-  
-let timer = setTimeout(removeClass, 100);
-  
-function removeClass(){
-body.classList.remove("is-scrolling");
-} */
+var ovni = gsap.from(".ovni" , {
+  y:20,
+  rotation:3,
+  duration:1.5,
+  repeat:-1,
+  yoyo: true,
+})
+
+
+/*Chapitre1-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+var planete1 = gsap.from(".planete01" , {
+  rotation:-360,duration:150,repeat:-1,ease:"none"
+})  
+
+var planete2 = gsap.from(".planete02" , {
+  rotation:360,duration:150,repeat:-1,ease:"none"
+})  
+
+/*Chapitre2-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+var planete3 = gsap.from(".planete03" , {
+  rotation:360,duration:150,repeat:-1,ease:"none"
+})  
+
+var fusee = gsap.fromTo(".fusee",{x:"-400%", y:"100"},{x:"1900%", y:"-1000",duration:"10"},{y:"-400%"},{y:"1900%"})  
+
+var planete4 = gsap.from(".planete04" , {
+  rotation:-360,duration:150,repeat:-1,ease:"none"
+})  
+
+/*Chapitre3-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+var planete5 = gsap.from(".planete05" , {
+  rotation:-360,duration:150,repeat:-1,ease:"none"
+})  
+
+/*Chapitre4-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+  var baleines = gsap.timeline()
+  .from(".baleine.no1", { x:"400%",duration:3 })
+  .from(".baleine.no2", { x:"400%",duration:3 }, 0.5)
+  .from(".baleine.no3", { x:"500%",duration:2} )
+  .from(".baleine.no4", { x:"400%",duration:3} )
+  .from(".baleine.no5", { x:"400%",duration:3} );
+
+/*Chapitre5-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+var asteroidPetit = gsap.timeline()
+.fromTo(".asteroid1", { x:"1100%",rotation:"0"},{ x:"-400%",rotation:"-360",duration:10 },"<0.25")
+.fromTo(".asteroid2", { x:"1100%",rotation:"0"},{ x:"-300%",rotation:"-360",duration:10 },"<0.25")
+.fromTo(".asteroid3", { x:"1400%",rotation:"0"},{ x:"-1700%",rotation:"-360",duration:10 },"<0.25")
+
+/*Chapitre6-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+  var brisement = gsap.timeline()
+  .from(".planete.Briser1", { y:"-70%",duration:3}, "<0.5")
+  .from(".planete.Briser2", { x:"400%",duration:3}, "<0.3")
+  .from(".planete.Briser3", { y:"400%",duration:3}, "<0.4")
+  .from(".planete.Briser4", { x:"-400%",duration:3}, "<0.5");    
+
+  brisement.reverse(0);
+
+/*Chapitre7-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+var asteroidGros = gsap.timeline()
+.fromTo(".asteroid4", { x:"1100%",rotation:"0"},{ x:"-200%",rotation:"-360",duration:10 },"<0.25")
+.fromTo(".asteroid5", { x:"1100%",rotation:"0"},{ x:"-800%",rotation:"-360",duration:10 },"<0.25")
+.fromTo(".asteroid6", { x:"1400%",rotation:"0"},{ x:"-1700%",rotation:"-360",duration:10 },"<0.25")
+.fromTo(".asteroid7", { x:"1100%",rotation:"0"},{ x:"-400%",rotation:"-360",duration:10 },"<0.25")
+.fromTo(".asteroid8", { x:"1100%",rotation:"0"},{ x:"-400%",rotation:"-360",duration:10 },"<0.25")
+.fromTo(".asteroid9", { x:"1400%",rotation:"0"},{ x:"-1700%",rotation:"-360",duration:10 },"<0.25")
+
+/*Chapitre8-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+var cthulu = gsap.from(".cthulu" , {
+  x:20,
+  opacity:0,
+  duration:2,
+})
