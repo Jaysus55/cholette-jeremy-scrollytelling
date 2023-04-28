833e4b099c79bf436cf272a07caa1791bfc06b30
Jaysus55 <oogiewoogie0@gmail.com>
2023-04-27 20:24:44 -0400

    Continuation de 2.2b
    Ajout des médias
    modification des noms de fichiers
    ajustement des consignes du 2.2a

--- script/script.js
+++ script/script.js
@@ -12,4 +12,17 @@ gsap.from(".CTA" , {
   })  
 
 
+/*let body = document.querySelector(".scroll");
+ 
+
+window.addEventListener("scroll", function(){
+body.classList.add("is-scrolling");
+window.clearTimeout(timer);
+timer = setTimeout(removeClass, 100);
+});
   
+let timer = setTimeout(removeClass, 100);
+  
+function removeClass(){
+body.classList.remove("is-scrolling");
+} */
