a962b0b9af097ccab3b5dccf9e9f6ce213edeb7b
Jaysus55 <114602010+Jaysus55@users.noreply.github.com>
2023-05-11 16:24:57 -0400

    p2.3a-b-c

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(MotionPathPlugin);
-gsap.registerPlugin(DrawSVGPlugin);
+/*gsap.registerPlugin(DrawSVGPlugin);*/
+/*gsap.registerPlugin(MorphSVGPlugin);*/
 
 
 var CTA = gsap.from(".CTA" , {
@@ -30,10 +31,10 @@ gsap.timeline({ scrollTrigger: {
   pin: true,
   markers: true,
   start: 'top top',
-  end: '300% bottom',
+  end: '500% bottom',
   trigger: 'header',
 }})
-.fromTo("#dessin",{ drawSVG: "0% 0%" },{drawSVG: "0% 100%",duration: 100,ease: "power1.inOut"})
+/*.fromTo("#draw",{ drawSVG: "0% 0%" },{drawSVG: "0% 100%",duration: 300,ease: "power1.inOut"})*/
 .from(".terre" , {rotation:360,duration:150,ease:"none",}) 
 
 
@@ -51,9 +52,10 @@ gsap.timeline({ scrollTrigger: {
   end: '300% bottom',
   trigger: '#chapitre1',
 }})
+/*.to("#fleche", {duration: 100, morphSVG:{shape:"#sourire", shapeIndex:5}},'<')*/
 .from(".planete02" , {rotation:360,duration:150,ease:"none"})
 .from(".planete01" , {rotation:-360,duration:150,ease:"none"}, '<')
-.to("#chapitre1", {backgroundPosition: "50% 100%",ease: "none",})
+.to("#chapitre1", {backgroundPosition: "50% 100%",ease: "none",});
 
 /*Chapitre2-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -99,32 +101,32 @@ body.classList.remove("is-scrolling");
 /*Chapitre4-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
   var baleines = gsap.timeline({scrollTrigger:{
-    scrub: true,
     pin: true,
     markers: true,
     start: 'top top',
-    end: '400% bottom',
+    end: '500% bottom',
     trigger: '#chapitre4',  
+    toggleActions: 'play pause resume pause',
   }})
-  .from(".baleine.no1", { x:"400%",duration:5 })
-  .from(".baleine.no2", { x:"400%",duration:8 }, 0.5)
-  .from(".baleine.no3", { x:"500%",duration:4} )
-  .from(".baleine.no4", { x:"400%",duration:5} )
-  .from(".baleine.no5", { x:"400%",duration:5} );
+  .fromTo(".baleine.no1",  { x:"300%"},{ x:"-200%",duration:3,ease:"none",repeat:-1})
+  .fromTo(".baleine.no2",  { x:"100%"},{ x:"-340%",duration:7,ease:"none",repeat:-1})
+  .fromTo(".baleine.no3",  { x:"300%"},{ x:"-330%",duration:3,ease:"none",repeat:-1})
+  .fromTo(".baleine.no4",  { x:"200%"},{ x:"-220%",duration:3,ease:"none",repeat:-1})
+  .fromTo(".baleine.no5",  { x:"450%"},{ x:"-200%",duration:15,ease:"none",repeat:-1});
 
 /*Chapitre5-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 var asteroidPetit = gsap.timeline({scrollTrigger:{
-  scrub: true,
   pin: true,
   markers: true,
   start: 'top top',
-  end: '400% bottom',
+  end: '300% bottom',
   trigger: '#chapitre5',  
+  toggleActions: 'play pause reverse pause',
 }})
-.fromTo(".asteroid1", { x:"1100%",rotation:"0"},{ x:"-400%",rotation:"-360",duration:10 },"<0.25")
-.fromTo(".asteroid2", { x:"1100%",rotation:"0"},{ x:"-300%",rotation:"-360",duration:10 },"<0.25")
-.fromTo(".asteroid3", { x:"1400%",rotation:"0"},{ x:"-1700%",rotation:"-360",duration:10 },"<0.25")
+.fromTo(".asteroid1", { x:"1100%",rotation:"0"},{ x:"-400%",rotation:"-360",duration:3 },"<0.25")
+.fromTo(".asteroid2", { x:"1100%",rotation:"0"},{ x:"-300%",rotation:"-360",duration:3 },"<0.25")
+.fromTo(".asteroid3", { x:"1400%",rotation:"0"},{ x:"-1700%",rotation:"-360",duration:3 },"<0.25")
 
 /*Chapitre6-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -142,8 +144,9 @@ var asteroidPetit = gsap.timeline({scrollTrigger:{
   .from(".planete.Briser4", { x:"-400%",duration:3}, "<0.5");    
 
   brisement.reverse(0);
-
 /*Chapitre7-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+const chapterHeight7 = document.getElementById("chapitre7").scrollHeight;
 
 var asteroidGros = gsap.timeline({scrollTrigger:{
   scrub: true,
@@ -153,12 +156,25 @@ var asteroidGros = gsap.timeline({scrollTrigger:{
   end: '400% bottom',
   trigger: '#chapitre7',  
 }})
-.fromTo(".asteroid4", { x:"1100%",rotation:"0"},{ x:"-200%",rotation:"-360",duration:50 },"<0.25")
+.fromTo(".asteroid4", { x:"1100%",y:chapterHeight7 * (-1 * -.1),rotation:"0"},{ x:"-200%",rotation:"-360",duration:50 },"<0.25")
 .fromTo(".asteroid5", { x:"1100%",rotation:"0"},{ x:"-800%",rotation:"-360",duration:50 },"<0.25")
 .fromTo(".asteroid6", { x:"1400%",rotation:"0"},{ x:"-1700%",rotation:"-360",duration:50 },"<0.25")
 .fromTo(".asteroid7", { x:"1100%",rotation:"0"},{ x:"-400%",rotation:"-360",duration:50 },"<0.25")
-.fromTo(".asteroid8", { x:"1100%",rotation:"0"},{ x:"-400%",rotation:"-360",duration:50 },"<0.25")
-.fromTo(".asteroid9", { x:"1400%",rotation:"0"},{ x:"-1700%",rotation:"-360",duration:50 },"<0.25")
+.fromTo(".asteroid8", { x:"1100%",y:chapterHeight7 * (-1 * .4),rotation:"0"},{ x:"-400%",rotation:"-360",duration:50 },"<0.25")
+.fromTo(".asteroid9", { x:"1400%",y:chapterHeight7 * (-1 * .1),rotation:"0"},{ x:"-1700%",rotation:"-360",duration:50 },"<0.25");
+
+gsap.to("#chapitre7", {
+  backgroundPosition: "50% 20%",
+  ease: "none",
+  scrollTrigger: {
+      trigger: "#chapitre7",
+      start: "top bottom",
+      end: "500% top",
+      scrub: true,
+      markers: true,
+      duration:100,
+  }
+});
 
 /*Chapitre8-------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
